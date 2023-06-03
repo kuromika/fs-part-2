a5381c12d30d3a2360116a49dd248a53347c8667
kuromika <kuromika.dev@gmail.com>
2023-06-03 11:55:20 -0500

    Modularize course component - 2.5

--- course-information/src/App.js
+++ course-information/src/App.js
@@ -1,33 +1,4 @@
-
-
-const Course = ({ course }) => {
-
-  return (
-    <div>
-      <Header name={course.name}></Header>
-      {course.parts.map((part) => {
-        return(<Part name={part.name} key={part.id} exercises={part.exercises}></Part>)
-      })}
-      <Total parts={course.parts}></Total>
-    </div>
-  )
-}
-
-const Header = ({ name }) => {
-  return <h1>{name}</h1>
-}
-
-const Part = ({ name, exercises }) => {
-  return (
-    <p>{name} {exercises}</p>
-  )
-}
-
-const Total = ({ parts }) => {
-  return <p>{parts.reduce((accumulator, current) => {
-    return accumulator + current.exercises;
-  }, 0 )}</p>
-}
+import Course from './components/Course'
 
 
 const App = () => {
