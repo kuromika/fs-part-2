00cd99099e8aa4eb024029469ae036fab21ac16e
kuromika <kuromika.dev@gmail.com>
2023-06-03 14:03:30 -0500

    Allow adding new persons - 2.6

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -6,19 +6,36 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setPersons(persons.concat({
+      name: newName
+    }));
+    setNewName('');
+  }
+
+  const handleInput = (event) => {
+    setNewName(event.target.value);
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          name: <input type='text' value={newName} onChange={handleInput} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map((person) => {
+        return (
+          <p key={person.name}>{person.name}</p>
+        )
+      })}
     </div>
   )
 }
